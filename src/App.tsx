2782f640ea35d7910df0662bd33527fb4aabfeae
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-10 21:05:02 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,179 +1,231 @@
-// import { useState } from "react";
-
-import { useState } from "react";
-
-// const Reply = ({ reply }) => {
-//   return (
-//     <div className="mb-4">
-//       <div className="shadow-md rounded-md p-4 mb-4 bg-red-500 w-[200px]">
-//         {reply}
-//       </div>
-//       <form>
-//         <input type="text" placeholder="reply..." />
-//       </form>
-//     </div>
-//   );
-// };
-// const Comment = ({ comment, postedComments, setPostedComments }) => {
-//   const [reply, setReply] = useState("");
-//   const [replies, setReplies] = useState(comment.replies);
-//   const addReply = () => {
-//     setPostedComments((prev) => {
-//       return prev.map((p) => {
-//         if (p.id !== comment.id) return p;
-//         else
-//           return {
-//             id: p.id,
-//             comment: p.comment,
-//             replies: [
-//               ...p.replies,
-//               {
-//                 id: crypto.randomUUID(),
-//                 replies: [],
-//                 reply: reply,
-//               },
-//             ],
-//           };
-//       });
-//     });
-//     setReply("");
-//   };
-//   return (
-//     <div className="shadow-md rounded-md p-4 mb-4 bg-red-500 w-[200px]">
-//       <div>{comment.comment}</div>
-//       <div>
-//         <div>
-//           {replies.map((reply) => {
-//             return <Reply reply={reply.reply} key={crypto.randomUUID()} />;
-//           })}
-//         </div>
-//         <div>
-//           <form>
-//             <input
-//               type="text"
-//               placeholder="Enter a reply to comment..."
-//               value={reply}
-//               onChange={(e) => setReply(e.target.value)}
-//             />
-//             <button type="button" onClick={addReply}>
-//               Reply
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   const [comment, setComment] = useState("");
-//   const [postedComments, setPostedComments] = useState([]); // Use state for posted comments
-//   console.log(postedComments);
-//   const addNewComment = () => {
-//     setPostedComments([
-//       ...postedComments,
-//       {
-//         id: crypto.randomUUID(),
-//         comment: comment,
-//         replies: [],
-//       },
-//     ]);
-//     setComment("");
-//   };
-
-//   return (
-//     <div>
-//       <div>
-//         <h1>Posted Comments</h1>
-//         <div>
-//           {postedComments.map((postedComment) => (
-//             <Comment
-//               key={crypto.randomUUID()}
-//               setPostedComments={setPostedComments}
-//               comment={postedComment}
-//               postedComments={postedComments}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//       <div>
-//         <form>
-//           <input
-//             type="text"
-//             placeholder="Enter a new comment..."
-//             value={comment}
-//             onChange={(e) => setComment(e.target.value)}
-//           />
-//           <button type="button" onClick={addNewComment}>
-//             Comment
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-const Comment = ({ comment, level }) => {
-  console.log(level + 1 + "px");
-  if (comment) {
-    return (
-      <div style={{ marginLeft: level * 30 + "px", marginTop: "10px" }}>
-        <div>
-          <div className="w-[200px] flex gap-4 p-4 bg-red-400 rounded-md shadow-md">
-            <div className="font-bold text-white">{comment.user}</div>
-            <div>{comment.text}</div>
-          </div>
-          <div>
-            <input type="text" placeholder="Reply..." />
-          </div>
+import { useRef, useState } from "react";
+const initialComments = [
+  {
+    name: "momsfav_01",
+    id: 1,
+    title: "Comment1",
+    reply: [
+      {
+        name: "pasang_12",
+        id: 11,
+        title: "C1 R1",
+        reply: [
+          {
+            name: "gopu_12",
+            id: 111,
+            title: "C1 R11",
+            reply: [],
+          },
+          {
+            name: "anushka_sen",
+            id: 112,
+            title: "C1 R12",
+            reply: [],
+          },
+        ],
+      },
+      {
+        name: "rii",
+        id: 12,
+        title: "C1 R12",
+        reply: [
+          {
+            name: "bhoju_123",
+            id: 121,
+            title: "C1 R21",
+            reply: [],
+          },
+          {
+            name: "ramesh_123",
+            id: 122,
+            title: "C1 R22",
+            reply: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "harish_rauf",
+    id: 2,
+    title: "Comment2",
+    reply: [
+      {
+        name: "krishna_123",
+        id: 21,
+        title: "C2 R1",
+        reply: [
+          {
+            name: "shyam_koirala",
+            id: 211,
+            title: "C1 R11",
+            reply: [],
+          },
+          {
+            name: "kalyan",
+            id: 212,
+            title: "C1 R12",
+            reply: [],
+          },
+        ],
+      },
+      {
+        name: "ritika",
+        id: 22,
+        title: "C2 R2",
+        reply: [
+          {
+            name: "mahesh",
+            id: 221,
+            title: "C2 R21",
+            reply: [],
+          },
+          {
+            name: "hiru",
+            id: 222,
+            title: "C2 R22",
+            reply: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+const Comment = ({ comment, level, helper }) => {
+  const [show, setShow] = useState(false);
+  const [showReply, setShowReply] = useState(false);
+  const replyRef = useRef();
+  return (
+    <div
+      style={{
+        marginLeft: level * 30 + "px",
+        marginBottom: "20px",
+        marginTop: "20px",
+      }}
+    >
+      <div
+        className="bg-red-500 flex items-center gap-4 text-white p-4 shadow-md "
+        style={{ width: "1000px" }}
+      >
+        <div className="font-bold">{comment.name}</div>
+        <div
+          style={{
+            padding: "10px",
+            background: "green",
+            borderRadius: "10px",
+            minWidth: "400px",
+          }}
+        >
+          {comment.title}
         </div>
-        <Comment comment={comment.reply} level={level + 1} />
+        <div>
+          <button
+            onClick={() => setShow((p) => !p)}
+            className="bg-green-500 rounded-md p-2"
+          >
+            show
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setShowReply((p) => !p);
+            }}
+            className="bg-green-500 rounded-md p-2"
+          >
+            reply
+          </button>
+        </div>
       </div>
-    );
-  }
+      {show && (
+        <div>
+          {comment.reply.map((comm) => {
+            return (
+              <Comment
+                comment={comm}
+                key={comm.id}
+                level={level + 1}
+                helper={helper}
+              />
+            );
+          })}
+        </div>
+      )}
+      {(showReply || show) && (
+        <div
+          className="py-4"
+          style={{ marginTop: "16px", marginBottom: "16px" }}
+        >
+          <input placeholder="reply...." className="border" ref={replyRef} />
+          <button
+            className="p-2 bg-green-500"
+            onClick={() => {
+              const replyText = replyRef.current.value;
+              helper(comment.id, replyText);
+              replyRef.current.value = "";
+              setShow(true);
+            }}
+          >
+            Reply
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
+const user = "riyaj_b1";
 const App = () => {
-  const [comment, setComment] = useState({
-    text: "This is the comment 1.",
-    user: "Riyaj Bhandari",
-    reply: {
-      text: "This is the reply 1.",
-      user: "Ramesh Shrestha",
-      reply: {
-        text: "This is the reply to reply1.",
-        user: "Gopi nath",
-        reply: {
-          text: "This is the reply to reply1 reply.",
-          user: "Ram nath Nepal",
-          reply: undefined,
-        },
-      },
-    },
-  });
-  function addNewReply() {
-    let temp = comment;
-    function helper(temp) {
-      if (temp.reply === undefined) {
-        temp.reply = {
-          text: "From the function.",
-          user: "Haris Rauf",
-        };
-        return;
-      } else helper(temp.reply);
+  const [comments, setComments] = useState(initialComments);
+  function helper(id, replyText) {
+    let temp = comments;
+    let found = false;
+    function findById(id, localComments, replyText) {
+      for (let comment of localComments) {
+        if (!found) {
+          if (comment.id === id) {
+            comment.reply.unshift({
+              name: user,
+              id: Math.floor(Math.random() * 10000),
+              title: replyText,
+              reply: [],
+            });
+
+            found = true;
+            return;
+          }
+          findById(id, comment.reply, replyText);
+        }
+      }
     }
-    helper(temp);
-    setComment({ ...temp });
+    findById(id, comments, replyText);
+    setComments([...temp]);
   }
+
   return (
-    <div>
-      <Comment comment={comment} level={0} />
+    <>
+      <h1 className="text-center font-bold" style={{ fontSize: "50px" }}>
+        Nested Comment System By RB(rii98 github)
+      </h1>
       <div>
-        <button onClick={addNewReply}>Add new reply</button>
+        {comments.map((comment) => {
+          return (
+            <div
+              className="shadow-md rounded-md p-6"
+              style={{ background: "black" }}
+            >
+              <Comment
+                comments={comments}
+                comment={comment}
+                key={comment.id}
+                level={0}
+                helper={helper}
+              />
+            </div>
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 };
 
